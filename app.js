e042f1693bd9e77a6f12efa084c982bf85b67f30
monambengouta <monam.bengouta@esprit.tn>
2022-05-09 12:34:21 +0100

    remove allow credential

--- app.js
+++ app.js
@@ -54,7 +54,6 @@ var app = express();
 app.use(cors())
 app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader('Access-Control-Allow-Credentials', 'true');
     res.setHeader(
         'Access-Control-Allow-Headers',
         'Origin, X-Requested-With, Content-Type, Accept, Authorization'
