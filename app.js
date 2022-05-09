f9c01743bae741567fc1f16cfadaa7316668ceac
monambengouta <monam.bengouta@esprit.tn>
2022-05-09 12:22:33 +0100

    allow credential

--- app.js
+++ app.js
@@ -54,6 +54,7 @@ var app = express();
 app.use(cors())
 app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Credentials', 'true');
     res.setHeader(
         'Access-Control-Allow-Headers',
         'Origin, X-Requested-With, Content-Type, Accept, Authorization'
